063df955d3174a53a7796c62e9297400dd04aa07
Otavio Medeiros <otaviorm@gmail.com>
2016-06-28 11:05:15 -0300

    node serving static content for SPA

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var notesRouter = require('./app/routes/notes');
 
+app.use('/', express.static(__dirname + '/public'));
 app.use('/api/notes', notesRouter);
 
 app.listen(3000);
