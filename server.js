31e2ee15746b3619aaa8c83a44a07bc958a12e6a
Otavio Medeiros <otaviorm@gmail.com>
2016-06-30 09:45:20 -0300

    added body-parser to node app

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var notesRouter = require('./app/routes/notes');
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
 
 app.use('/', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 app.use('/api/notebooks', notesRouter);
 
 app.listen(process.env.APP_PORT);
