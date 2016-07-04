536e1ad3c7a144923e2d4e5f9bc7ee38eb811769
Otavio Medeiros <otaviorm@gmail.com>
2016-07-04 14:22:49 +0000

    saving notes with tags

--- public/app.js
+++ public/app.js
@@ -122,12 +122,13 @@ notesApp.controller('editNoteController', ['$scope', '$http', '$routeParams', fu
 
   $http.get('/api/notes/' + $routeParams.id).then(function(result){
     $scope.note = result.data;
+    
   });
 
   $scope.loadTags = function(query) {
     return $http.get('/api/tags').then(function(result){
       return result.data.map(function(item){
-        return {text: item.name, id: item._id};
+        return {text: item.name, tagId: item._id};
       });
     });
 
