876cf5f13c05c8d7cbdb93ee9a86f852890be8ef
Otavio Medeiros <otaviorm@gmail.com>
2016-07-06 13:50:47 +0000

    login endpoint

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var User = require('../models/user');
 
 router.post('/register', function(req, res){
@@ -12,6 +13,23 @@ router.post('/register', function(req, res){
   user.save(function(err) {
     res.json({ "token": user.generateJwt() });
   });
+});
+
+router.post('/login', function(req, res){
+  passport.authenticate('local', function(err, user, info){
+    var token;
+
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+
+    if(user){
+      res.json({ "token" : user.generateJwt() });
+    } else {
+      res.status(401).json(info);
+    }
+  })(req, res);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var notebooksRouter = require('./app/routes/notebooks');
 var notesRouter = require('./app/routes/notes');
 var tagsRouter = require('./app/routes/tags');
 var authenticationRouter = require('./app/routes/authentication');
+require('./app/config/passport');
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
 
 app.use('/', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
 
 app.use('/api/authentication', authenticationRouter);
 
