0b0ffa0c2d2a40c5a3696d2b17321a29ec4bd78f
Otavio Medeiros <otaviorm@gmail.com>
2016-07-06 10:37:29 +0000

    method to generating JWT in user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -28,6 +29,18 @@ userSchema.methods.validPassword = function(password) {
 
 userSchema.methods.generateHash = function(password, salt){
   return crypto.pbkdf2Sync(password, salt, 1000, 64).toString('hex');
-}
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 1);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, process.env.JWT_SECRET); 
+};
 
 module.exports = mongoose.model('User', userSchema);
